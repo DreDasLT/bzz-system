bb886f964e88a0634391c03619d74d67921a9a69
Vilius Antanavičius <vilius@idenfy.com>
2020-09-17 23:28:08 +0300

    Swagger documentation tool

--- backend/src/core/routes/index.ts
+++ backend/src/core/routes/index.ts
@@ -4,7 +4,6 @@ import priceRouter from '../../entities/prices/price.routes';
 import modelRouter from '../../entities/models/model.routes';
 
 const router = Router()
-
 router.use('/car', carRouter)
 router.use('/price', priceRouter)
 router.use('/model', modelRouter)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,17 @@ import router from './core/routes'
 
 const PORT = process.env.PORT || 3000
 
+
 const app = express()
 app.use(express.json())
 
-app.use('/', router)
+app.use('/api', router)
 
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+ 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Started on: http://localhost:${PORT}`)
